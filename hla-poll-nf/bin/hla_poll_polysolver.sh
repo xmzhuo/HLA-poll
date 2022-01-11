b340a6fca1bd1bacd9b5abff60d57b35b0e449d7
xmzhuo <xmzhuo@gmail.com>
2022-01-10 20:44:19 -0500

    Delete hla-poll-nf directory

--- hla-poll-nf/bin/hla_poll_polysolver.sh
+++
@@ -1,9 +0,0 @@
-
-sample=$1
-sample=${sample%.bam}
-
-/home/polysolver/scripts/shell_call_hla_type $sample.bam Unknown 1 hg38 STDFQ 0 $sample
-        
-cp $sample/winners.hla.txt $sample.polysolver.result
-
-cat $sample.polysolver.result | sed 's/[a-z]*_[a-z]_//g' | sed 's/_/:/g' | awk '{print $1"*"$2"\n"$1"*"$3}' > $sample.polysolver.f.result
